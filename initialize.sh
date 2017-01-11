e9582c01ada8fa420aa79710545035c5a71cb988
afeiship <1290657123@qq.com>
2017-01-12 04:11:56 +0800

    update aliases

--- initialize.sh
+++ initialize.sh
@@ -2,17 +2,20 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+# if centos:
+# yum install -y openssl-devel readline-devel zlib-devel;
+
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv;
 # 用来编译安装 ruby
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build;
 # 用来管理 gemset, 可选, 因为有 bundler 也没什么必要
-git clone git://github.com/jamis/rbenv-gemset.git  ~/.rbenv/plugins/rbenv-gemset
+git clone git://github.com/jamis/rbenv-gemset.git  ~/.rbenv/plugins/rbenv-gemset;
 # 通过 gem 命令安装完 gem 后无需手动输入 rbenv rehash 命令, 推荐
-git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash;
 # 通过 rbenv update 命令来更新 rbenv 以及所有插件, 推荐
-git clone git://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
+git clone git://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update;
 # 使用 Ruby China 的镜像安装 Ruby, 国内用户推荐
-git clone git://github.com/AndorChen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror
+git clone git://github.com/AndorChen/rbenv-china-mirror.git ~/.rbenv/plugins/rbenv-china-mirror;
 
 
 unset ROOT_PATH;
