3879e7dc337ed1bed046d787f05b418b6b867a21
afeiship <1290657123@qq.com>
2017-01-12 04:12:12 +0800

    pdate aliaeese

--- index.sh
+++ index.sh
@@ -5,10 +5,10 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 export PATH="$HOME/.rbenv/bin:$PATH";
 eval "$(rbenv init -)";
 
-
+## aliases:
 alias rbenv-list='rbenv install --list';
 alias rbenv-i='rbenv install';
 alias rbenv-vers='rbenv versions';
-alias rbenv-global='rbenv global'
+alias rbenv-global='rbenv global';
 
 unset ROOT_PATH;
