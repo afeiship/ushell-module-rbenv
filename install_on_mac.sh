92785208aad9cdf62eea3af0f1d75100c6a63fed
afeiship <1290657123@qq.com>
2019-02-23 12:09:03 +0800

    add: install on mac

--- install_on_mac.sh
+++ install_on_mac.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 ## https://github.com/rbenv/rbenv#installation
 
+## step1: install
+brew install rbenv
+
+## step2: put to your shell:
+# rbenv init
+
+## step3: check(open new terminal window)
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
